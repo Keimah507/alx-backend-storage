fe2a4a89070d8f934f1bdf25d2f7ac31d3a8cf67
keimah507 <githinjibrian5@gmail.com>
2023-01-11 10:12:53 +0300

    add 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,7 @@
+--MySQL Advanced task 0
+--Creates a table Users
 CREATE TABLE Users IF NOT EXISTS(
     PRIMARY KEY(id int NOT NULL AUTO INCREMENT ), 
     email CHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
     );
