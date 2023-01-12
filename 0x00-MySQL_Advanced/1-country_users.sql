4d22c0abaa5db21407190d6b4a31a971758af174
keimah507 <githinjibrian5@gmail.com>
2023-01-12 22:24:00 +0300

    Fix line 4

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- MySQL Advanced task 1
 -- Creates a table Users
 CREATE TABLE IF NOT EXISTS Users(
-    PRIMARY KEY(id int NOT NULL AUTO INCREMENT),
-    email CHAR(255) NOT NULL UNIQUE,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN' DEFAULT 'US'),
-)
+    country ENUM('US', 'CO', 'TN' DEFAULT 'US')
+);
