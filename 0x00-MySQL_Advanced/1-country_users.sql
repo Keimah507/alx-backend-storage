bff2e04fae3f33c43d4f92fb98ef864493bcae1f
keimah507 <githinjibrian5@gmail.com>
2023-01-12 22:25:02 +0300

    change table name to users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- MySQL Advanced task 1
 -- Creates a table Users
-CREATE TABLE IF NOT EXISTS Users(
+CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
