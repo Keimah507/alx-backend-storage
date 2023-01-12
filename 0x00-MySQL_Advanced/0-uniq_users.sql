507d495fed312bf8a9fe78d244ab6dd9f37714dd
keimah507 <githinjibrian5@gmail.com>
2023-01-12 22:28:55 +0300

    fix bug on lines 3 and 4

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- MySQL Advanced task 0
 -- Creates a table Users
-CREATE TABLE Users IF NOT EXISTS(
-    PRIMARY KEY(id int NOT NULL AUTO INCREMENT ), 
-    email CHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
     );
