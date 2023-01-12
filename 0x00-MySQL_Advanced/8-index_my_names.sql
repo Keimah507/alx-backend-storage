5f6f5f69335f3282166df9b264c382edaccafd3d
keimah507 <githinjibrian5@gmail.com>
2023-01-12 22:36:12 +0300

    Change typo on line 3

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
 -- creates an index idx_names_first on the table names and
 -- first letter of the name
-CREATE INDEX idx_names_first ON names(name(1));
+CREATE INDEX idx_name_first ON names(name(1));
