08b4b18bf47bb7378d68982d546d958fa0583fd1
keimah507 <githinjibrian5@gmail.com>
2023-01-17 11:35:02 +0300

    upload files

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
-    print("{} logs".format(collection.estimated_document_count))
+    print("{} logs".format(collection.estimated_document_count()))
     print("Methods:")
     print("\tmethod GET: {}".format(collection.count_documents({"method": "GET"})))
     print("\tmethod POST: {}".format(collection.count_documents({"method": "POST"})))
